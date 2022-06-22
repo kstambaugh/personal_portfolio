8afae1ddc02c6f70eb59439a77ce5a5df4d253ce
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-06-21 20:29:58 -0400

    adding styles

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius
+    }
+    circumference() {
+        return Math.PI * this.radius * 2;
+    }
+    area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+const circleMaker = () => {
+
+}
